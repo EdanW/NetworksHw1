62ebccb0a6c4c1cac3098b74a9004d9426bde75d
EdanW <edanweinberg@gmail.com>
2024-11-14 11:04:44 +0200

    added more skeleton and some renames

--- numbers_server.py
+++ numbers_server.py
@@ -5,6 +5,40 @@ import select
 import sys
 
 
+def print_login_message(client_socket):
+    message = 'Welcome! Please log in'
+    client_socket.send(message.encode())
+
+def command_handler(conn_socket, message):
+    command = message.split(' ')[0]
+    print(message, command)
+    if command == 'max:':
+        print('max')
+    elif command == 'calculate:':
+        print('calc')
+    elif command == 'factors:':
+        print('factors')
+    elif command == 'quit':
+        if message != 'quit':
+            send_invalid_input_error(conn_socket)
+        disconnect_socket(conn_socket)
+
+def send_invalid_input_error(conn_socket):
+    print(conn_socket)
+    # todo write this func (send to conn_socket the message via send_all?)
+
+def recv_all(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        return data
+    # Note: I hope this works as expected.
+
+def disconnect_socket(conn_socket):
+    sockets_list.remove(conn_socket)
+    del clients[conn_socket]
+    conn_socket.close()
 
 def process_file(file_name, users_dic):
     try:
@@ -17,6 +51,9 @@ def process_file(file_name, users_dic):
         print('error in processing file')
         exit(1)
 
+# Global vars
+sockets_list = []
+clients = {}  # Dictionary to keep track of client addresses
 
 def start_server():
     # Server Configuration
@@ -27,10 +64,10 @@ def start_server():
         exit(1)
     if len(sys.argv) == 2:
         PORT = sys.argv[2]
-    file_name = sys.argv[1]
+    file_path = sys.argv[1]
     #loads all the users information to a dictionary
     known_users_dict = {}
-    process_file(file_name,known_users_dict)
+    process_file(file_path,known_users_dict)
 
     logged_in_users_sockets_list = []
 
@@ -40,8 +77,7 @@ def start_server():
     server_socket.listen()
 
     # Step 5: Use lists to keep track of sockets
-    sockets_list = [server_socket]  # List of all connected sockets
-    clients = {}  # Dictionary to keep track of client addresses
+    sockets_list.append(server_socket) # List of all connected sockets
 
     # Step 6: Main loop to handle multiple clients
     while True:
@@ -49,9 +85,9 @@ def start_server():
         read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
         # Step 8: Iterate over readable sockets
-        for notified_socket in read_sockets:
+        for active_socket in read_sockets:
             # Step 9: If it's the server socket, accept a new connection
-            if notified_socket == server_socket:
+            if active_socket == server_socket:
                 client_socket, client_address = server_socket.accept()
                 sockets_list.append(client_socket)
                 clients[client_socket] = client_address
@@ -59,39 +95,35 @@ def start_server():
 
             # Step 10: Otherwise, it's an existing client socket with data
             else:
-
-                message = notified_socket.recv(1024)  # Receive up to 1024 byte
+                message = recv_all(active_socket)  # Receive full message
                 decoded_message = message.decode("utf-8")
-                # todo understand how to receive entire message even if its every long
 
                 # Step 10.5 if youre not logged in, do so. if you dont i kick u
-                if client_socket not in logged_in_users_sockets_list:
+                if active_socket not in logged_in_users_sockets_list:
                     print('placeholder')
+                    is_valid_user = False;
                     #todo find out how to validate logging in
-                # Step 11: Receive data from the client
+                    if is_valid_user:
+                        logged_in_users_sockets_list.append(active_socket)
+                    else:
+                        disconnect_socket(active_socket)
+                        continue
 
                 # Step 12: If no data is received, the client has disconnected
-                if not message:
-                    sockets_list.remove(notified_socket)
-                    del clients[notified_socket]
-                    notified_socket.close()  # Closes the connection properly
+                if not decoded_message:
+                    disconnect_socket(active_socket)
                     continue
 
                 # Step 13: If data is received, USE HANDLER
+                else:
+                    command_handler(active_socket, decoded_message)
 
 
         # Step 14: Handle any exceptional conditions on sockets
-        for notified_socket in exception_sockets:
-            sockets_list.remove(notified_socket)
-            del clients[notified_socket]
+        for active_socket in exception_sockets:
+            disconnect_socket(active_socket)
 
 
-def print_login_message(client_socket):
-    message = 'Welcome! Please log in'
-    client_socket.send(message.encode())
-
-def command_handler():
-    print('placeholder')
 
 
 if __name__ == "__main__":

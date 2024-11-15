7f2d7c10391c580b5f5113cea89674d04c58453b
dinasim <dina2@mail.tau.ac.il>
2024-11-15 11:47:15 +0200

    added math functions and some logic to the server function: dictionary for the messages, $ that symbols the end of the message and all the consequences that result from it,

--- numbers_server.py
+++ numbers_server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import math
 from socket import *
 import select
 import sys
@@ -10,35 +10,92 @@ def print_login_message(client_socket):
     client_socket.send(message.encode())
 
 def command_handler(conn_socket, message):
-    command = message.split(' ')[0]
+    data = message.split(':')[0]
+    command = data[0]
+    expression = data[1]
     print(message, command)
     if command == 'max:':
         print('max')
+        return maximum(expression)
     elif command == 'calculate:':
         print('calc')
+        return  calculate(expression)
     elif command == 'factors:':
         print('factors')
+        return get_prime_factors(expression)
     elif command == 'quit':
-        if message != 'quit':
-            send_invalid_input_error(conn_socket)
-        disconnect_socket(conn_socket)
+        return 'quit'
 
 def send_invalid_input_error(conn_socket):
     print(conn_socket)
     # todo write this func (send to conn_socket the message via send_all?)
 
-def recv_all(client_socket):
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        return data
-    # Note: I hope this works as expected.
-
 def disconnect_socket(conn_socket):
     sockets_list.remove(conn_socket)
     del clients[conn_socket]
     conn_socket.close()
+
+def calculate(expression):
+    """
+    @pre: the form of expression is "X "operation" Y"
+    @pre: if the math operation is "/", Y!=0
+    """
+    expr_arr = expression.split()
+    #converts the numbers from string to int
+    expr_arr[0] = int(expr_arr[0])
+    expr_arr[2] = int(expr_arr[2])
+
+    if expression[1] == "+":
+            return  str(expr_arr[0] + expr_arr[2])
+    elif expression[1] == "-":
+            return str(expr_arr[0] - expr_arr[2])
+    elif expression[1] == "*":
+            return  str(expr_arr[0] * expr_arr[2])
+    elif  expression[1] == "/":
+            return  str(expr_arr[0] / expr_arr[2])
+    elif expression[1] == "^":
+            return str(math.pow(expr_arr[0],expr_arr[2]))
+
+
+def maximum(numbers):
+    numbers_arr = [int(x) for x in numbers.split(" ")]
+    return str(max(numbers_arr))
+
+
+def is_prime(n):
+    """Checks if a number is prime"""
+    if n == 1:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    #Checks if there is a number up to the range of the root of n that divides n
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def get_prime_factors(n):
+    """
+    returns a set of all the prime factors of n
+    """
+    factors = set()
+
+    while n % 2 == 0:
+        factors.add(2)
+        n //= 2
+
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        while n % i == 0:
+            if is_prime(i):
+                factors.add(i)
+            n //= i
+
+    return str(factors)
+
 
 def process_file(file_name, users_dic):
     try:
@@ -51,9 +108,19 @@ def process_file(file_name, users_dic):
         print('error in processing file')
         exit(1)
 
+def proccess_login_data(login_info):
+    """
+    read login info and return a tuple of username and password
+    """
+    #todo decide in which format the client send us the information so we'll
+    # know how to break and use it
+    return None
+
+
 # Global vars
 sockets_list = []
 clients = {}  # Dictionary to keep track of client addresses
+clients_messages = {}
 
 def start_server():
     # Server Configuration
@@ -95,31 +162,48 @@ def start_server():
 
             # Step 10: Otherwise, it's an existing client socket with data
             else:
-                message = recv_all(active_socket)  # Receive full message
-                decoded_message = message.decode("utf-8")
+                #gets the address of the active socket for dictionary searches
+                client_address = clients[active_socket]
 
-                # Step 10.5 if youre not logged in, do so. if you dont i kick u
-                if active_socket not in logged_in_users_sockets_list:
-                    print('placeholder')
-                    is_valid_user = False;
-                    #todo find out how to validate logging in
-                    if is_valid_user:
-                        logged_in_users_sockets_list.append(active_socket)
-                    else:
-                        disconnect_socket(active_socket)
+                if client_address not in clients_messages:
+                    clients_messages[client_address]=""
+                clients_messages[client_address] += active_socket.recv(1024)
+
+                message_builder = clients_messages[client_address].decode("utf-8")
+
+                #check if we got the full message
+                if message_builder[-1] == "$":
+                    # Step 10.5 if you're not logged in, do so. if you don't i kick u
+                    if active_socket not in logged_in_users_sockets_list:
+
+                        username, password = proccess_login_data(message_builder)
+
+                        if username in known_users_dict and known_users_dict[username] == password:
+                            logged_in_users_sockets_list.append(active_socket)
+                        else:
+                            #the user can try again so no need to close the socket
+                            active_socket.send("Failed to login.".encode("utf-8"))
                         continue
 
-                # Step 12: If no data is received, the client has disconnected
-                if not decoded_message:
-                    disconnect_socket(active_socket)
-                    continue
+                    # Step 13: you're logged in, so you probably sent us some command
+                    else:
+                        #handle this case first because it requires removing stuff from data structures
+                        if message_builder[:-2] == 'quit':
+                            send_invalid_input_error(active_socket)
+                            clients_messages.pop(client_address)
+                            logged_in_users_sockets_list.remove(active_socket)
+                            clients.pop(active_socket)
+                            disconnect_socket(active_socket)
 
-                # Step 13: If data is received, USE HANDLER
-                else:
-                    command_handler(active_socket, decoded_message)
+                        message_to_send = command_handler(active_socket, message_builder)
+                        active_socket.send(message_to_send.encode("utf-8"))
+
+                    #ready for new command
+                    clients_messages[client_address] = ""
 
 
-        # Step 14: Handle any exceptional conditions on sockets
+
+                    # Step 14: Handle any exceptional conditions on sockets
         for active_socket in exception_sockets:
             disconnect_socket(active_socket)
 

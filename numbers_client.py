3234014a88583aaeb0d120c7f7a12c28d9dc41ff
edanw <edanweinberg@gmail.com>
2024-11-20 12:42:39 +0200

    finished login

--- numbers_client.py
+++ numbers_client.py
@@ -3,8 +3,24 @@
 from socket import *
 import sys
 
+
+def recv_all_as_string(client_socket):
+    msg = ''
+    while msg[-1] != '\t':
+        tmp = client_socket.recv(1024).decode('utf-8')
+        # Handle FIN packet case
+        if not tmp:
+            client_socket.close()
+            return ''
+        msg += tmp
+    return msg[:-1]
+
+
+def validate_login_input(user, pw):
+    return user[0:7] == "User: " and pw[0:11] == "Password: "
+
+
 def start_client():
-    print('h')
     HOST_NAME = 'localhost'
     PORT = 1337
 
@@ -23,27 +39,46 @@ def start_client():
     client_socket = socket(AF_INET, SOCK_STREAM)
     client_socket.connect((HOST_NAME, PORT))
 
-    msg = client_socket.recv(1024).decode('utf-8')
-    if msg != 'Welcome! Please log in.':
-        print('Did not receive proper welcoming message from server')
+    welcome_msg = client_socket.recv(1024).decode('utf-8')
+    if welcome_msg != 'Welcome! Please log in.':
+        print('Did not receive proper welcoming message from server, shutting down connection')
         client_socket.close()
         exit(1)
 
-
+    # Login loop
+    successful_login = False
     while True:
-        msg = input() # input from cli/terminal
-        msg += '$' # protocol
-        client_socket.sendall(msg.encode('utf-8'))
-
-        response = client_socket.recv(1024).decode('utf-8')
-        # todo our own recv_all based on $ protocol
-        if not response: # FIN
+        user_input = input()
+        password_input = input()
+        if not validate_login_input(user_input, password_input):
+            print("Unexpected login format")
             client_socket.close()
             break
+        user_input = user_input[7:]
+        password_input = password_input[11:]
+        login_msg = user_input + '\n' + password_input + '\t'
+        client_socket.sendall(login_msg.encode('utf-8'))
 
-        print(response) # note- should our recv_all return bytes or string
+        response = recv_all_as_string(client_socket).decode('utf-8')
+
+        if response == "Failed to login.":
+            print(response)
+        else:
+            if not response:
+                # Handle FIN case
+                break
+            elif response == f"Hi {user_input}, good to see you":
+                print(response)
+                successful_login = True
+            else:
+                print(f"Internal error: unexpected server behavior. response was:{response}")
+            break
 
 
+
+    # Methods loops
+    while successful_login:
+        print('h')
 
 
 if __name__ == "__main__":
